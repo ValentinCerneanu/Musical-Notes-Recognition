beefdf810bbc9ec4f9f274c9b6d3bd649e3d28e2
Cosmin Musteata <cosmin.g.musteata@gmail.com>
2020-05-20 22:38:17 +0300

    sound test 2.0

--- mydsp.py
+++ mydsp.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sounddevice as sd
+import scipy.io.wavfile as wav
 import matplotlib.pyplot as plt
 def mySine(A, f, theta0, t):
     x=A*np.sin(2*np.pi*f*t+theta0)
@@ -42,3 +43,23 @@ def myRamp(t,m,ad):
         if t[i] >= -ad:
             y[i] = m*(t[i] + ad)
     return y
+def find_pitch(audiofile):
+    fs,x = wav.read(audiofile)
+    ms20=int((fs/50))
+    ms2=int(fs/500)
+
+    x=[i/32767 for i in x]
+
+    y=plt.acorr(x,maxlags=ms20,normed=True)
+
+    y=y[1]
+    z=y[round(len(y)/2):]
+    z=z[ms2:ms20]
+    zmax=max(z)
+
+    index=np.where(z==zmax)
+    index=index[0][0]
+
+    pitch=fs/(ms2+index+2)
+
+    return pitch
--- project.py
+++ project.py
@@ -1,11 +1,13 @@
 import sounddevice as sd
 from scipy.io.wavfile import write
-
+import pitch
+import librosa
+import librosa.display
 from scipy.io import wavfile
-import scipy.signal as sp
+# import scipy.signal as sp
 import numpy as np
 import matplotlib.pyplot as plt
-import time
+# import time
 import mydsp
 
 fs=44100
@@ -15,14 +17,61 @@ print("recording...............")
 
 record_voice=sd.rec(int(duration * fs),samplerate=fs,channels=2)
 sd.wait()       
-write("sound.wav",fs,record_voice)
+write("sound_E4.wav",fs,record_voice)
 
-fs, x = wavfile.read('sound.wav')
-plt.figure(num=None, figsize=(8, 6), dpi=80, facecolor='w', edgecolor='k')
-plt.close('all')
-mydsp.plotInTime(x,fs)
-plt.title('Original Signal')
-mydsp.plotInFrequency(x,fs)
-plt.title('Spectrum for the Original Signal')
+# wave_data, samplerate=librosa.load('sound.wav')
 
+# plt.subplot(211)
+# plt.plot(wave_data) 
+# plt.title('wave')
+# pitches, magnitudes = librosa.piptrack(y=wave_data, sr=samplerate)
+# plt.subplot(212)
+# plt.imshow(pitches[:100, :], aspect="auto", interpolation="nearest", origin="bottom")
+# plt.title('pitches')
+y, sr = librosa.load('sound.wav')
+
+# Set the hop length; at 22050 Hz, 512 samples ~= 23ms
+hop_length = 512
+
+# Separate harmonics and percussives into two waveforms
+y_harmonic, y_percussive = librosa.effects.hpss(y)
+
+# Beat track on the percussive signal
+tempo, beat_frames = librosa.beat.beat_track(y=y_percussive,
+                                             sr=sr)
+
+# Compute MFCC features from the raw signal
+mfcc = librosa.feature.mfcc(y=y, sr=sr, hop_length=hop_length, n_mfcc=13)
+
+# And the first-order differences (delta features)
+mfcc_delta = librosa.feature.delta(mfcc)
+
+# Stack and synchronize between beat events
+# This time, we'll use the mean value (default) instead of median
+beat_mfcc_delta = librosa.util.sync(np.vstack([mfcc, mfcc_delta]),
+                                    beat_frames)
+
+# Compute chroma features from the harmonic signal
+chromagram = librosa.feature.chroma_cqt(y=y_harmonic,
+                                        sr=sr)
+plt.figure()
+librosa.display.specshow(chromagram, y_axis='chroma', x_axis='time')
+plt.title('chroma_cqt')
+plt.colorbar()
+plt.tight_layout()
+plt.show()
+
+
+# p = mydsp.find_pitch('sound.wav')
+# print('pitch =', p)
+
+# fs, x = wavfile.read('sound.wav')
+# plt.close('all')
+# plt.figure()
+# mydsp.plotInTime(x,fs)
+# plt.title('Original Signal')
+# plt.figure()
+# mydsp.plotInFrequency(x,fs)
+# plt.title('Spectrum for the Original Signal')
+# mydsp.myPlay(x,fs)
 
