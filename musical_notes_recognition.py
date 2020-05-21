47287cc6714df86f207d62eb52413eca0cead136
ValentinCerneanu <valentincerneanu28@gmail.com>
2020-05-21 19:43:07 +0300

    desired note

--- musical_notes_recognition.py
+++ musical_notes_recognition.py
@@ -3,6 +3,26 @@ from scipy.io.wavfile import write
 import librosa
 import librosa.display
 import matplotlib.pyplot as plt
+
+switcherStringToIndex = {
+        "C":0,
+        "C#":1,
+        "D":2,
+        "D#":3,
+        "E":4,
+        "F":5,
+        "F#":6,
+        "G":7,
+        "G#":8,
+        "A":9,
+        "A#":10,
+        "B":11,
+    }
+
+desiredNote = input("Enter your desired note : ") 
+indexDesiredNote = int(switcherStringToIndex.get(desiredNote, -1));
+
+
 fs=44100
 duration=4
 print("recording...............")
@@ -10,9 +30,9 @@ print("recording...............")
 # record input sound from microphone
 record_voice=sd.rec(int(duration * fs),samplerate=fs,channels=2)
 sd.wait()       
-write("sound_test_440.wav",fs,record_voice)
+write("sound_recorded.wav",fs,record_voice)
 
-y, sr = librosa.load('sound_E4.wav')
+y, sr = librosa.load('sound_recorded.wav')
 
 # Separate harmonics and percussives into two waveforms
 y_harmonic, y_percussive = librosa.effects.hpss(y)
@@ -52,8 +72,8 @@ for i in range(length):
         maxFreq = freqOfNotes[i]
         maxFreqIndex = i;
 
-print(freqOfNotes)
-print(maxFreqIndex)
+#print(freqOfNotes)
+#print(maxFreqIndex)
 
 switcher = {
         0: "C",
@@ -72,3 +92,10 @@ switcher = {
 
 print("Musical note recognized is: ", switcher.get(maxFreqIndex, "Invalid note"))
 
+
+if maxFreqIndex > indexDesiredNote:
+    print("Too high")
+elif maxFreqIndex < indexDesiredNote:
+    print("Too low")
+else:
+    print("Perfect")
